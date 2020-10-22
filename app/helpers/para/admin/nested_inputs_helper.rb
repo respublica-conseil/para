39b20599e701c80218f8bb23eb30979b298e5ddc
vala <gonjo@free.fr>
2020-10-22 11:36:37 +0200

    Make nested inputs subclass instance get its parent association filled when built from add buttons

---
+++ app/helpers/para/admin/nested_inputs_helper.rb
@@ -0,0 +1,17 @@
+module Para
+  module Admin
+    module NestedInputsHelper
+      # Helper that allows filling a parent association for a given resource, based on the
+      # inverse_of option of the parent resource association.
+      #
+      def with_inverse_association_for(resource, attribute_name, parent_resource)
+        resource.tap do
+          association_name = parent_resource.association(attribute_name).options[:inverse_of]
+          return resource unless association_name
+
+          resource.association(association_name).replace(parent_resource)
+        end
+      end
+    end
+  end
+end
